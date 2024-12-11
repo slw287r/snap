be9f99ecac2cb2c4680364046db92492ad2b29ef
slw287r <slw287r@163.com>
2024-12-11 10:03:28 +0800

    finish HSK reads counts for T2T

--- SNAPLib/version.h
+++ SNAPLib/version.h
@@ -1,8 +1,2 @@
 #pragma once
-#define BRANCH_COMMIT "dev 49c61cc"
-f BRANCH_COMMIT
-#define BRANCH_COMMIT "dev 847d0d1"
-#undef BRANCH_COMMIT
-#define BRANCH_COMMIT "dev 027c49c"
-#undef BRANCH_COMMIT
 #define BRANCH_COMMIT "dev 7689e33"
