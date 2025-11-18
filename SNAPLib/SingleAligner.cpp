d872baf008c408562aa749397e11b709f0403f35
slw287r <slw287r@163.com>
2025-11-18 09:09:42 +0800

    prepare to parse IC regions on the fly

--- SNAPLib/SingleAligner.cpp
+++ SNAPLib/SingleAligner.cpp
@@ -362,6 +362,8 @@ SingleAlignerContext::runIterationThreadImpl(Read *& read)
             }
             if (hasIC)
             {
+				// [DEBUG]
+				PP
                 auto it = icpos.find(alignmentResults[0].location);
                 if (it != icpos.end())
                 {
